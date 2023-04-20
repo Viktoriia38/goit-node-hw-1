4457106f62914d84e298afc43c36b2b032814324
Viktoriia38 <bva922@ukr.net>
2023-04-20 19:52:03 +0300

    Made files contacts.js, index.js, add packege.json

---
+++ contacts.js
@@ -0,0 +1,23 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+// const contactsPath = ;
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+// Написати зміну на 4 рядку через пат, створити функції та зробити експорт через модуль
